1410dcbe29415405ddfde293de9d9fb3b3363006
Behi7 <behruzbekkt2@gmail.com>
2024-08-07 22:25:45 +0500

    up

--- api/urls.py
+++ api/urls.py
@@ -8,11 +8,15 @@
 #     path('murojatlar', views.contactList, name='contact'),
 # ]
 from django.urls import path
-from .views import ContactList, BannerList, InfoList, CellList
+from .views import ContactList, BannerList, InfoList, CellList, ContactDetail, BannerDetail, InfoDetail, CellDetail
 
 urlpatterns = [
     path('contacts/', ContactList.as_view({'get': 'list'})),
     path('banners/', BannerList.as_view({'get': 'list'})),
-    path('info', InfoList.as_view({'get': 'list'})),
-    path('cell', CellList.as_view({'get': 'list'}))
+    path('info/', InfoList.as_view({'get': 'list'})),
+    path('cell/', CellList.as_view({'get': 'list'})),
+    path('contacts/<int:pk>', ContactDetail.as_view()),
+    path('banners/<int:pk>', BannerDetail.as_view()),
+    path('info/<int:pk>', InfoDetail.as_view()),
+    path('cell/<int:pk>', CellDetail.as_view())
 ]
--- api/views.py
+++ api/views.py
@@ -1,14 +1,32 @@
 from rest_framework import viewsets
 from .serializers import ContactSerializer, BannerSerializer, InfoSerializer, CellSerializer
 from main.models import Banner, Contact, Cell, Info
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 class ContactList(viewsets.ModelViewSet):
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
 
-class BannerList(viewsets.ModelViewSet):
+
+class ContactDetail(APIView):
+    def get(self, request, pk):
+        contact = Contact.objects.get(pk=pk)
+        serializer = ContactSerializer(contact)
+        return Response(serializer.data)
+
+
+class BannerList(viewsets.ModelViewSet): 
     queryset = Banner.objects.all()
     serializer_class = BannerSerializer
+
+
+class BannerDetail(APIView):
+    def get(self, request, pk):
+        contact = Banner.objects.get(pk=pk)
+        serializer = BannerSerializer(contact)
+        return Response(serializer.data)
 
 
 class InfoList(viewsets.ModelViewSet):
@@ -16,6 +34,20 @@ class InfoList(viewsets.ModelViewSet):
     serializer_class = InfoSerializer
 
 
+class InfoDetail(APIView):
+    def get(self, request, pk):
+        contact = Info.objects.get(pk=pk)
+        serializer = InfoSerializer(contact)
+        return Response(serializer.data)
+
+
 class CellList(viewsets.ModelViewSet):
     queryset = Cell.objects.all()
     serializer_class = CellSerializer
+
+
+class CellDetail(APIView):
+    def get(self, request, pk):
+        contact = Cell.objects.get(pk=pk)
+        serializer = CellSerializer(contact)
+        return Response(serializer.data)
